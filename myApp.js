7ca39716733ac81d9491b21013a21d0eaf4d68d7
Gisleude Linhares <gisleudenem@live.com>
2022-02-28 17:01:58 -0300

    frameguard question solution

--- myApp.js
+++ myApp.js
@@ -1,56 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const app = express();
 
+//hide PoweredBy from Header
+app.use(helmet.hidePoweredBy());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//mitigating possible clickjacking
+app.use(helmet.frameguard({action: 'deny'}));
 
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
+
 app.disable('strict-transport-security');
+
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
